9380120011bcb22ef53820149ccf810fa04c95ae
heningm <13699448253@163.com>
2024-09-10 00:22:28 +0800

    moble menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,9 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import NavLink from './NavLink'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import MenuOverlay from './MenuOverlay'
 
 
 const navLinks = [
@@ -19,11 +22,27 @@ const navLinks = [
 ]
 
 const NavBar = () => {
+    const [narbarOpen, setNarbarOpen] = useState(false);
+
     return (
-        <nav className='fixed top-0 right-0 left-0 z-10 bg-[#121212]  bg-opacity-90'>
-            <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
+        <nav className='fixed top-0 right-0 left-0 z-10 bg-[#121212]  bg-opacity-95'>
+            <div className='flex flex-wrap items-center justify-between mx-auto px-4 pt-2'>
                 <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
-                <div className='menu block md:w-auto' id='navbar'>
+
+                <div className='moblie-menu block md:hidden'>
+                    {
+                        !narbarOpen ? (
+                            <button onClick={() => setNarbarOpen(true)} className=' flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                                <Bars3Icon className='h-5 w-5' />
+                            </button>
+                        ) : (
+                            <button onClick={() => setNarbarOpen(false)} className=' flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                                <XMarkIcon className='h-5 w-5' />
+                            </button>
+                        )
+                    }
+                </div>
+                <div className='menu hidden md:block md:w-auto' id='navbar'>
                     <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 md:mt-0'>
                         {navLinks.map((link, index) => (
                             <li key={index}>
@@ -33,6 +52,9 @@ const NavBar = () => {
                     </ul>
                 </div>
             </div>
+            {
+                narbarOpen ? <MenuOverlay links={navLinks} /> : null
+            }
         </nav>
     )
 }
