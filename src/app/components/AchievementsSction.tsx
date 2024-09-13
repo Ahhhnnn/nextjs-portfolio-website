11a7a12afe0e5b59281eea939350e3860c06232b
heningm <13699448253@163.com>
2024-09-14 00:50:10 +0800

    Acheivements Section

---
+++ src/app/components/AchievementsSction.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+// achievementsList
+const achievementsList = [
+  {
+    metric: "Projects",
+    value: "100",
+    postfix: "+",
+  },
+  {
+    prefix: "~",
+    metric: "Users",
+    value: "100,000",
+  },
+  {
+    metric: "Awards",
+    value: "7",
+  },
+  {
+    metric: "Years",
+    value: "5",
+  },
+];
+
+const AchievementsSction = () => {
+  return (
+    <div className="py-8 px-4 sm:py-16 xl:px-16">
+      <div className="sm:border-[#33353F] sm:border rounded-lg py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+        {achievementsList.map((achievement, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+            >
+              <h2 className="text-white text-4xl font-bold">
+                {achievement.prefix}
+                {achievement.value}
+                {achievement.postfix}
+              </h2>
+              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AchievementsSction;
